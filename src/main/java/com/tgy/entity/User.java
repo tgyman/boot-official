17f715c9507e6f983bfa48325a7ca797d78a21f6
Tong <179495813@qq.com>
2020-07-05 23:21:44 +0800

    用户类

---
+++ src/main/java/com/tgy/entity/User.java
@@ -0,0 +1,34 @@
+package com.tgy.entity;
+
+/**
+ * 用户类
+ */
+public class User {
+    private String userName;//用户姓名
+    private int age;//年龄
+    private String address;//家庭住址
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
